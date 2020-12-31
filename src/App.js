37376836a3a827fdaffa6debef762eb9820ec374
Monis Khan <63677714+Iammonis@users.noreply.github.com>
2021-01-01 01:22:55 +0530

    Employer auth (#23)
    merging

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Provider } from 'react-redux';
 import './App.css';
-import { Login } from './Components/Recruiter/Register/Login';
-import { Register } from './Components/Recruiter/Register/Register';
 import {  Router } from './Components/Route/Route';
 import { store } from './Redux/store';
-
 
 function App() {
   return (
@@ -12,10 +9,8 @@ function App() {
     <div className="App">
       <h1>New Project</h1>
       
-      {/* <SearchBar/>
-      <Router/> */}
-      {/* <Register/> */}
-      <Login/>
+      <SearchBar/>
+      <Router/>
     </div>
     </Provider>
   );
