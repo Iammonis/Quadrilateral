b07958c71072a918ad4cd5677fcb9ef277788e4b
krishnasai <yamanikrishnasai45@gmail.com>
2020-12-31 22:44:44 +0530

    recruiter register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux';
 import './App.css';
+import { Register } from './Components/Recruiter/Register/Register';
 import {  Router } from './Components/Route/Route';
 import { SearchBar } from './Components/SearchBar/SearchBar';
 import { store } from './Redux/store';
@@ -10,8 +11,9 @@ function App() {
     <div className="App">
       <h1>New Project</h1>
       
-      <SearchBar/>
-      <Router/>
+      {/* <SearchBar/>
+      <Router/> */}
+      <Register/>
     </div>
     </Provider>
   );
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { loginReducer } from "./LoginRedux/loginReducer";
+import { getRegisterReducer } from "./RecruiterRegister/GetRegister/getReducer";
+import { postRegisterReducer } from "./RecruiterRegister/PostRegister/postReducer";
 // import {thunk} from 'thunk'
 
 
 
-const rootReducer = combineReducers({log:loginReducer})
+const rootReducer = combineReducers({log:loginReducer,get:getRegisterReducer,post : postRegisterReducer})
 let thunk = ({ getState, dispatch }) => (next) => (action) => {
     if (typeof action === "function") {
       return action(dispatch, getState);
